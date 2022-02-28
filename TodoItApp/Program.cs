d7468bf41da8d1dc264419e2d5fabe5185a02914
jorgenpe <82228083+jorgenpe@users.noreply.github.com>
2022-02-28 13:53:37 +0100

    Add project files.

---
+++ TodoItApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoItApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
