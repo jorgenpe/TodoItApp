fe8b5510a06156795f11a570cd3d019a746d37be
jorgenpe <82228083+jorgenpe@users.noreply.github.com>
2022-03-01 11:31:39 +0100

    up to date

---
+++ TodoIt.Tests/Models/TodoTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+using TodoItApp.Models;
+
+namespace TodoIt.Tests
+{
+    public class TodoTests
+    {
+        [Fact]
+        public void TodoIdTest()
+        {
+            Todo todo = new Todo(1, "testDescription");
+        }
+    }
+}
--- TodoItApp/Models/Todo.cs
+++ TodoItApp/Models/Todo.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace TodoItApp.Models
 {
-    internal class Class1
+    public class Todo
     {
+        private int todoId;
+        private string description;
+        private bool done;
+        private Person assignee;
+
+
+        public Todo(int id, string description)
+        {
+            this.todoId = id;
+            this.description = description;
+        }
+
+        public Todo(int id, string description, bool done, Person assignee)
+        {
+            this.todoId=id;
+            this.description = description;
+            this.done = done;
+            this.assignee = assignee;
+        }
+
+        public int TodoId { get { return todoId; } }
+        public string Description { get { return description; } set { description = value; } }    
+        public bool Done { get { return done; } set { done = value; } }
+        public Person Assignee { get { return assignee; } set { assignee = value; } }   
+
     }
 }
