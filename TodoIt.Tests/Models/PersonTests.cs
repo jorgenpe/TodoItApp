2904a5fd53a3f1f0580c74a4d93a17f76e2adc2b
jorgenpe <82228083+jorgenpe@users.noreply.github.com>
2022-03-01 09:52:32 +0100

    Created PersonTests class

---
+++ TodoIt.Tests/Models/PersonTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Xunit;
+using TodoItApp.Models;
+
+namespace TodoIt.Tests
+{
+    public class PersonTests
+    {
+        [Fact]
+        public void PersonIdTest()
+        {
+            Person person = new Person(1);
+            int testValue = 0;
+            Assert.NotEqual(testValue, person.PersonId);
+            Assert.Equal(1, person.PersonId);
+        }
+
+        [Fact]
+        public void PersonFirstNameTest()
+        {
+            Person person = new Person(1);
+            person.FirstName = "Test";
+            string result = "Test";
+            Assert.NotNull(person.FirstName);
+            Assert.Equal(result, person.FirstName);
+            Assert.NotEqual("", person.FirstName);
+        }
+
+        [Fact]
+        public void PersonLastNameTest()
+        {
+            Person person = new Person(1);
+            person.LastName = "Test";
+            string result = "Test";
+            Assert.NotNull(person.LastName);
+            Assert.Equal(result, person.LastName);
+            Assert.NotEqual("", person.LastName);
+        }
+
+
+        [Fact]
+        
+        public void TestFirstNameThrowsNullReferenceException()
+        {
+            //Arrange
+            string expected = "The first name can't be empty or null!";
+            Person person = new Person(1, "testFirstName", "testLastname");
+            string firstName = null;
+            //Act and Assert
+            Assert.Throws<NullReferenceException>(() => person.FirstName = firstName );
+            
+
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+
+        public void TestFirstNameThrowsException(string firstName)
+        {
+            //Arrange
+            Person person = new Person(1, "testFirstName", "testLastname");
+            //Act and Assert
+            Assert.Throws<ArgumentNullException>(() => person.FirstName = firstName);
+            
+
+        }
+    }
+}
