72ff496b791cf1c130f28f7b7f3c6b54bcf045a6
jorgenpe <82228083+jorgenpe@users.noreply.github.com>
2022-03-01 12:50:28 +0100

    TodoSequencer and TodoSequencerTests created

---
+++ TodoIt.Tests/Data/TodoSequncerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using TodoItApp.Data;
+
+namespace TodoIt.Tests.Data
+{
+    public class TodoSequencerTests
+    {
+        [Fact]
+        public void TodoSequencerTest()
+        {
+
+            Assert.NotEqual(0, TodoSequencer.NextTodoId());
+            TodoSequencer.Reset();
+            Assert.Equal(1, TodoSequencer.NextTodoId());
+            Assert.Equal(2, TodoSequencer.NextTodoId());
+            Assert.Equal(3, TodoSequencer.NextTodoId());
+        }
+    }
+}
---
+++ TodoItApp/Data/TodoSequencer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoItApp.Data
+{
+    public class TodoSequencer
+    {
+        private static int todoId;
+
+
+        public static int NextTodoId()
+        {
+            return ++todoId;
+        }
+
+        public static void Reset()
+        {
+            todoId = 0;
+        }
+    }
+}
