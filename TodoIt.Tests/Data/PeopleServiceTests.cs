802d057665cc7f0b3ce5b23aa11762139c91b53a
jorgenpe <82228083+jorgenpe@users.noreply.github.com>
2022-03-01 16:30:10 +0100

    Created PersonService and PersonServiceTests

---
+++ TodoIt.Tests/Data/PeopleServiceTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoItApp.Data;
+using TodoItApp.Models;
+using Xunit;
+
+namespace TodoIt.Tests.Data
+{
+    public class PeopleServiceTests
+    {
+        [Fact]
+        public void SizeArrayTest()
+        {
+            PersonSequencer.Reset();
+            PeopleService personService = new PeopleService();
+            Assert.Equal(0, personService.Size());
+
+            personService.CreatePerson("firstNameOne", "lastNameOne");
+            personService.CreatePerson("firstNametwo", "lastNameTwo");
+            personService.CreatePerson("firstNameThree", "lastNameThree");
+            Assert.NotEqual(0, personService.Size());
+            Assert.Equal(3, personService.Size());
+        }
+        
+        [Fact]
+        public void FindAllTest()
+        {
+            PersonSequencer.Reset();
+            
+            PeopleService personService = new PeopleService();
+            personService.Clear();
+
+            Assert.Equal(0, personService.FindAll().Length);
+            
+            personService.CreatePerson("firstNameOne", "lastNameOne");
+            personService.CreatePerson("firstNametwo", "lastNameTwo");
+            personService.CreatePerson("firstNameThree", "lastNameThree");
+            personService.CreatePerson("firstNameFour", "lastNameFour");
+            Assert.NotEqual(0, personService.FindAll().Length);
+            Assert.Equal(4, personService.FindAll().Length);
+        }
+
+        [Fact]
+        public void FindByIdTest()
+        {
+            PersonSequencer.Reset();
+
+            PeopleService personService = new PeopleService();
+            personService.Clear();
+
+            personService.CreatePerson("firstNameOne", "lastNameOne");
+            personService.CreatePerson("firstNametwo", "lastNameTwo");
+            personService.CreatePerson("firstNameThree", "lastNameThree");
+            personService.CreatePerson("firstNameFour", "lastNameFour");
+
+            Person result = personService.FindById(2);
+
+            Assert.NotEqual(0, result.PersonId);
+            Assert.Equal(2, result.PersonId);
+        }
+
+
+        [Fact]
+        public void ClearTest()
+        {
+            PersonSequencer.Reset();
+
+            PeopleService personService = new PeopleService();
+            personService.Clear();
+
+            personService.CreatePerson("firstNameOne", "lastNameOne");
+            personService.CreatePerson("firstNametwo", "lastNameTwo");
+            personService.CreatePerson("firstNameThree", "lastNameThree");
+            personService.CreatePerson("firstNameFour", "lastNameFour");
+
+            Assert.NotEqual(0, personService.Size());
+            personService.Clear();
+            Assert.NotNull(personService);
+            Assert.Equal(0, personService.FindAll().Length);
+        }
+
+    }
+}
---
+++ TodoItApp/Data/PeopleService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoItApp.Models;
+
+namespace TodoItApp.Data
+{
+    public class PeopleService
+    {
+        private static Person[] persons = new Person[0];
+
+        public int Size()
+        {
+            return persons.Length;
+        }
+
+        public Person[] FindAll()
+        {
+            return persons;
+        }
+
+        public Person FindById(int personId)
+        {
+            for(int i = 0; i < persons.Length; i++)
+            {
+                if(persons[i].PersonId == personId)
+                {
+                    return persons[i];
+                }
+            }
+            return null;
+        }
+
+        public Person CreatePerson(string firstName, string lastName)
+        {
+            Person person = new Person(PersonSequencer.NextPersonId(),firstName,lastName);
+            
+            Array.Resize<Person>(ref persons , persons.Length + 1);
+            persons[persons.Length-1] = person;
+
+            return person;
+        }
+
+        public void Clear()
+        {
+            persons = new Person[0];
+        }
+    }
+}
